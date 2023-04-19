b02895ba353f802ed71650bd06ac34043ee1f0b9
20211tiimi0010 <caiofcintra@gmail.com>
2023-04-19 13:01:26 -0300

    Terminei o script

--- app/src/main/java/cintra/coelho/fraga/caio/galeria/adapter/MyAdapter.java
+++ app/src/main/java/cintra/coelho/fraga/caio/galeria/adapter/MyAdapter.java
@@ -1,12 +1,17 @@
 package cintra.coelho.fraga.caio.galeria.adapter;
 
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
+import cintra.coelho.fraga.caio.galeria.R;
 import cintra.coelho.fraga.caio.galeria.activity.MainActivity;
 import cintra.coelho.fraga.caio.galeria.model.MyItem;
 
@@ -23,16 +28,29 @@ public class MyAdapter extends RecyclerView.Adapter {
     @NonNull
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater inflater = LayoutInflater.from(mainActivity);
+        View v = inflater.inflate(R.layout.item_list,parent, false);
+        return new MyViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        MyItem myItem = itens.get(position);
 
+        View v = holder.itemView;
+
+        ImageView imvphoto = v.findViewById(R.id.imvPhoto);
+        imvphoto.setImageURI(myItem.photo);
+
+        TextView tvTitle = v.findViewById(R.id.tvTitle);
+        tvTitle.setText(myItem.title);
+
+        TextView tvdesc = v.findViewById(R.id.tvDesc);
+        tvdesc.setText(myItem.description);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return itens.size();
     }
 }
